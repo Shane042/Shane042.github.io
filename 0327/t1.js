218fd434170240ab8f4e25b47c9e0a33e7c7d589
Shane042 <shane.connell@ou.edu>
2024-03-27 10:20:30 -0500

    Update 0327

---
+++ 0327/t1.js
@@ -0,0 +1,44 @@
+"use strict";
+let R = null;
+
+//TypeScript = more rules to make it like C#
+
+let t1 = document.querySelector("#t1"); //local and global (because there are no curly brackets to surround t1)
+
+let b1 = document.querySelector("#b1");
+
+let g1 = null; // table
+
+async function loads()
+{
+    let url = `https://juxinglong.github.io/static/data/states.json`;
+
+    let r = await fetch(url);
+    let rj = await r.json();
+
+    if (g1 != null)
+    {
+        g1.destroy();
+    }
+    t1.innerHTML = ``;
+
+    let opts = {
+        data: rj,
+        width: 500,
+        pagination: { limit: 10 },
+        sort: true,
+        search: true,
+        columns: [{ id: "st", name: "State" }, {id:"p",name:"Population"}],
+    };
+
+    g1 = new gridjs.Grid(opts);
+    g1.render(t1);
+
+    R = rj;
+    console.log(rj);
+
+    //fetch(); //async
+    Swal.fire("Data loaded");
+};
+
+b1.addEventListener("click", loads );
